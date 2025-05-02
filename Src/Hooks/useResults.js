b015274183d5a29d517837d46205494c5afdf2e4
Emre Özer <eozer1371@gmail.com>
2025-05-03 00:57:31 +0300

    Final API adjustments and project finalized.

--- Src/Hooks/useResults.js
+++ Src/Hooks/useResults.js
@@ -3,14 +3,23 @@ import api from '../API/api';
 
 export default () => {
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
   const searchapi = async searchTerm => {
-    const response = await api.get('/search', {
-      params: {
-        limit: 50,
-        term: searchTerm,
-        location: 'İstanbul',
-      },
-    });
+
+    try {
+      const response = await api.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'İstanbul',
+        },
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    }
+    catch (error) {
+     setErrorMessage('Bağlantı Hatası!');
+    }
 
     setResults(response.data.businesses);
   };
--- Src/Screens/SearchScreen/index.js
+++ Src/Screens/SearchScreen/index.js
@@ -5,7 +5,7 @@ import ResultsList from '../../Components/ResultsList';
 import useResults from '../../Hooks/useResults';
 
 export default function SearchScreen() {
-  const [searchapi, results] = useResults();
+  const [searchapi, results, errorMessage] = useResults();
   const [term, setTerm] = useState('')
   console.log(results);
 
@@ -20,6 +20,8 @@ export default function SearchScreen() {
       term = {term} 
       onTermChange={setTerm}
       onTermSubmit={() => searchapi(term)}/>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      {results.length === 0 ? <Text>Yükleniyor...</Text> : null}
       <ResultsList title='Ucuz Restorantlar' results={filterResultsByPrice('₺')} />
       <ResultsList title='Uygun Restorantlar' results={filterResultsByPrice('₺₺')} />
       <ResultsList title='Pahalı Restorantlar' results={filterResultsByPrice('₺₺₺')} />
